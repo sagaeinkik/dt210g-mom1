dbf7e925b52a5fa3363cda44c16397d4ca2d63fd
Saga Einarsdotter Kikajon <saga@kikajon.se>
2025-02-07 12:52:15 +0100

    Lagt till bild i banner

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 //Scss
 import "./Header.scss"; 
+import banner from "../assets/images/banner.jpg"
 
 //Interface för att ta emot props
 interface HeaderProps {
@@ -12,9 +13,10 @@ const Header = ({ title }: HeaderProps ) => {
     <>
         <header>
           <div className="text-content">
-
-            <p>{ title }</p>
+            <p id="top">{ title }</p>
+            <p id="bottom">{ title }</p>
           </div>
+          <img src={banner} alt="Manchester-spår i pist" />
         </header>
     </>
   )
